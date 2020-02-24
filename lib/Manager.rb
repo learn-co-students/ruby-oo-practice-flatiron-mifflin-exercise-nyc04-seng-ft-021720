8e6f20d5a2350764528c2dca965bf2e11a4046e2
96StanleyLee <96stanleylee@gmail.com>
2020-02-24 12:33:36 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,48 @@
 class Manager
 
+    attr_accessor :name, :department, :age 
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name 
+        @department = department 
+        @age = age 
+        @@all.push(self)
+    end 
+
+
+    def employees
+        Employee.all.select do |employees|
+            employees.manager = self 
+        end 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def hire_employee(name, salary)
+        Employee.new(name,salary, self)
+    end 
+
+    def self.all_departments
+        self.all.map do |manager|
+            manager.department 
+        end 
+    end 
+
+    def self.average_age
+       average = self.all.map do |manager|
+            manager.age
+       end 
+       average.sum.to_f/self.all.count
+
+    end
+
+
+
+    
+
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,14 @@ require 'pry'
 
 #Test your code here
 
+stanley = Manager.new("Stanley", "HR", 23)
+katy = Manager.new("Katy", "IT", 24)
+ryan = Manager.new("Ryan", "Finance", 25)
 
-binding.pry
-puts "done"
+
+kevin = stanley.hire_employee('Kevin',50_000)
+daniel = stanley.hire_employee('Daniel', 80_000)
+stacey = katy.hire_employee('Stacey', 51_000)
+
+
+p kevin.tax_bracket
